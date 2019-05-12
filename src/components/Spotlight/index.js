d84ea1f546c7689c3105d136ecb31af688757c44
chris44f <f.chris@live.co.uk>
2019-05-12 03:18:27 +0100

    changed folder name

--- src/components/Spotlight/index.js
+++ src/components/Spotlight/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './index.css'
-import Image from '../image'
 import { TiStar } from 'react-icons/ti'
 import { TiStarOutline} from 'react-icons/ti'
 
@@ -8,7 +7,7 @@ class Spotlight extends Component {
 
   render() {
 
-    const { id, src, caption, category, handleFavourite, favourite, closeSpotlight } = this.props
+    const { id, src, caption, category, handleFavourite, closeSpotlight } = this.props
 
     const buttonFavourite = () => (
       this.props.favourite ?
@@ -20,7 +19,7 @@ class Spotlight extends Component {
       <div className="spotlight-wrapper">
         {buttonFavourite()}
         <h3 className="spotlight-category">It belongs to the category of {category}</h3>
-        <img src={src} />
+        <img src={src} alt=""/>
         <h2 className="spotlight-caption">{caption}</h2>
         <button className="x-button" onClick={()=>closeSpotlight()}>X</button>
       </div>
